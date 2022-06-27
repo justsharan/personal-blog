c8353d6566fcba13a611ac6a5efb39a17f1dc9ec
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-06-27 12:16:30 -0700

    Fix 404 errors

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 rm -rf $PWD/out
 npx next build
 npx next export
+mkdir $PWD/out/movies
+mv $PWD/out/movies.html $PWD/out/movies/index.html
+mv $PWD/out/posts.html $PWD/out/posts/index.html
 ssh root@justsharan.xyz 'rm -rf /var/www/html/justsharan.xyz'
 scp -r $PWD/out root@justsharan.xyz:/var/www/html/justsharan.xyz
 rm -rf $PWD/out
