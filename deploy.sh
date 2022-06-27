58b60509ead0f084a4aa9e13ed9ff4c0c14c10f0
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-06-26 18:48:59 -0700

    Remove animations since they break everything

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf $PWD/out
-yarn build
+npx next build
+npx next export
 ssh root@justsharan.xyz 'rm -rf /var/www/html/justsharan.xyz'
 scp -r $PWD/out root@justsharan.xyz:/var/www/html/justsharan.xyz
 rm -rf $PWD/out
